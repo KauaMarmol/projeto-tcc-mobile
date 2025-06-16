cf8ee9c9b3fbe90021538f435d6c1ecd2cf714f4
KauaMarmol <159555487+KauaMarmol@users.noreply.github.com>
2025-06-16 12:54:28 +0000

    [feat] Ajustes

--- App.js
+++ App.js
@@ -4,15 +4,33 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   const [data, setData] = useState([]);
+  /* const API_URL = "https://curly-space-trout-4vjgjx6qwq5hqp66-3000.app.github.dev" */
+  /* const API_URL = "http://localhost:3000" */
 
   const fetchData = async () => {
-    const API_URL = "https://curly-space-trout-4vjgjx6qwq5hqp66-3000.app.github.dev"
     const ENDPOINT = "/api/user"
+    const API_URL = "https://curly-space-trout-4vjgjx6qwq5hqp66-3000.app.github.dev"
     const response = await fetch(API_URL + ENDPOINT);
     const json = await response.json();
     setData(json);
   }
 
+  const postData = async () => {
+    const ENDPOINT = "/api/user";
+    const body = {
+      username: "usuario1",
+      email: "usuario1@teste.com.br"
+    }
+    const response = await fetch(API_URL + ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+  });
+  const json = await respone.json();
+  console.log(json);
+}
   return (
     <View style={styles.container}>
       <Text>Carregar Dados do Servidor</Text>
@@ -24,6 +42,7 @@ export default function App() {
         })
       }
       <Button title="Carregar Dados" onPress={fetchData}/>
+      <Button title="Enviar Dados" onPress={postData} />
       <StatusBar style="auto" />
     </View>
   );
