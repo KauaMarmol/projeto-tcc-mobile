9e19ef5fa7e2f329fe396ac6cebab03dc224440f
perosso <muriloeduborges@gmail.com>
2025-10-07 08:25:58 -0300

    Fix Botões

--- App.js
+++ App.js
@@ -11,27 +11,6 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
 
-  //Aula Grazi
-  const [data, setData] = useState([]);
-
-  const fetchData = async () => {
-    const API = "http://192.170.0.129:3000/api"; //substituir o ip pelo ip do ipconfig do backend
-    const URL = `${API}/user`;
-    console.log(URL);
-    setLoading(true);
-
-    try {
-      const response = await fetch(URL);
-      const json = await response.json();
-      console.log(json)
-    } catch (error) {
-      console.log(error)
-    } finally {
-      setLoading(false);
-    }
-  };
-  //Final
-
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false }}>
